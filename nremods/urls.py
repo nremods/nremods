33d12a27addf51461306db0f0abfc469effa8a58
nremods <ap117664@gmail.com>
2025-08-17 01:49:49 +0530

    Update urls.py

--- nremods/urls.py
+++ nremods/urls.py
@@ -1,22 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-from django.shortcuts import render
 from nremods import views
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home, name="home"),
-    path("shop/", shop, name="shop"),
-    path("cart/", cart, name="cart"),
+    path("", views.home, name="home"),
+    path("shop/", views.shop, name="shop"),
+    path("cart/", views.cart, name="cart"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("otp/", otp, name="otp"),
-    path("product/", product, name="product"),
-    path("pay/", pay, name="pay"),
-    path("get-mail/", get_mail, name="get_mail"),
+    path("otp/", views.otp, name="otp"),
+    path("product/", views.product, name="product"),
+    path("pay/", views.pay, name="pay"),
+    path("get-mail/", views.get_mail, name="get_mail"),
 ]
-
-
