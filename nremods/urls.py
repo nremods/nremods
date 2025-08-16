8f0c4f6bc5b2d46cfa86d3b94a9bead8249efe8c
nremods <ap117664@gmail.com>
2025-08-17 01:39:40 +0530

    Update urls.py

--- nremods/urls.py
+++ nremods/urls.py
@@ -3,40 +3,15 @@ from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html")
-
-def shop(request):
-    return render(request, "shop.html")
-
-def cart(request):
-    return render(request, "cart.html")
-
-def login_view(request):
-    return render(request, "login.html")
-
-def register(request):
-    return render(request, "register.html")
-
-def otp(request):
-    return render(request, "otp.html")
-
-def product(request):
-    return render(request, "product.html")
-
-def pay(request):
-    return render(request, "pay.html")
-
-def get_mail(request):
-    return render(request, "get_mail.html")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
     path("shop/", shop, name="shop"),
     path("cart/", cart, name="cart"),
-    path("login/", login_view, name="login"),
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path("otp/", otp, name="otp"),
     path("product/", product, name="product"),
     path("pay/", pay, name="pay"),
